7107e05412f1456a382e2e14b439f043470d9c4d
Nikolaenko <aleshka.dev@gmail.com>
2025-11-09 23:05:36 +0200

    delete prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
